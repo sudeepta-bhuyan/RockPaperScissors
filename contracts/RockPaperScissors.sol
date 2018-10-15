b5d654fe561fe0f929f3b71d129cbc66181ae2d8
sudeepta-bhuyan <sudeepta29@gmail.com>
2018-10-15 22:55:02 +0530

    addressed review comments

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -3,176 +3,166 @@ pragma solidity ^0.4.20;
 import './Pausable.sol';
 
 contract RockPaperScissors is Pausable {
-
+    
     enum Move {
-        None, Rock, Paper, Scissor
+        None, Rock, Paper, Scissors
     }
     
     enum GameState {
-        Enrollment,
-        LockMove,
-        RevealMove,
-        WinnerDecided
+        ChallengeIssued,
+        ChallengeAccepted,
+        WinnerDecided,
+        NoWinner,
+        GameOver
     }
     
-    struct Player {
-        address addr;
-        bytes32 hashedMove;
-        Move actualMove;
-        uint256 moneyWagered;
-        bool enrolled;
-        bool locked;
-        bool revealed;
+    struct Game {
+        
+        GameState state;
+        uint256 challengeIssuedTime; //using block number instead of timestamp
+        uint256 ChallengeAcceptedTime; //using block number instead of timestamp
+        address winner;
+        
+        address challenger;
+        uint256 challengerAmount;
+        bytes32 challengerSecretMove;
+        
+        address acceptor;
+        uint256 acceptorAmount;
+        Move acceptorMove;
     }
     
-    uint256 constant public minWager = 200; //wei
-    GameState public state = GameState.Enrollment;
-    address[2] public playerAddresses;
-    mapping(address => Player) public playerDetails;
+    uint256 constant public minWager = 200;
+    uint256 timeout = 100;
+    mapping(uint256=>Game) games;
+    uint256 numOfGames;
+    mapping(bytes32 => bool) public usedSecrets;
     
-    address public winner;
-    uint8 public enrolledCount;
-    uint8 public lockedCount;
-    uint8 public revealedCount;
-    mapping(bytes32 => bool) usedSecrets;
-    uint256 public amountWon;
+    event LogChallengeIssued(uint256 indexed gameID, address indexed challenger, uint256 amountWagered);
+    event LogChallengeAccepted(uint256 indexed gameID, address indexed acceptor, uint256 amountWagered);
+    event LogWinnerDecided(uint256 indexed gameID, address indexed winner, uint256 amountWon);
+    event LogClaimedWinningAmount(uint256 indexed gameID, address indexed who, uint256 amountWithdrawn);
+    event LogWithdrawnByChallenger(uint256 indexed gameID, address indexed who, uint256 amountWithdrawn);
+    event LogWithdrawnByAcceptor(uint256 indexed gameID, address indexed who, uint256 amountWithdrawn);
+    event LogReclaimedWageredAmount(uint256 indexed gameID, address indexed who, uint256 amountReclaimed);
     
-    event LogPlayerEnrolled(address indexed who, uint256 amountWagered);
-    event LogSecretMoveLocked(address indexed whose, bytes32 secretMove);
-    event LogMoveRevealed(address indexed whose, Move move);
-    event LogWinner(address indexed who, uint256 amountWon);
-    event LogNewRoundRequired(address indexed player1, address indexed player2);
-    event LogAmountWithdrawn(address indexed who, uint256 amount);
-    
-    modifier winnerOnly {
-        require(msg.sender == winner);
+    modifier validAmountOnly {
+        require(msg.value >= 200);
         _;
     }
     
-    function ChangeState() private {
-        if (state == GameState.WinnerDecided) {
-            state = GameState.Enrollment;
-        } else {
-            state = GameState(uint8(state) + 1);
-        }
-    }
-    
-    function NewRound() private {
-        state = GameState.LockMove;
-        
-        for (uint8 i = 0; i < 2; i++) {
-            address player = playerAddresses[i];
-            playerDetails[player].hashedMove = bytes32(0); //do we need to explicitly delete the previous value??
-            playerDetails[player].actualMove = Move.None;
-            playerDetails[player].locked = false;
-            playerDetails[player].revealed = false;
-        }
-    }
-    
-    function cleanup() private {
-        for (uint8 i = 0; i < 2; i++) {
-            address player = playerAddresses[i];
-            delete playerDetails[player];
-            playerAddresses[i] = 0;
-        }
-        enrolledCount = 0;
-        lockedCount = 0;
-        revealedCount = 0;
-    }
-    
-    function checkWinner() private {
-        address player1 = playerAddresses[0];
-        address player2 = playerAddresses[1];
-        
-        //Maybe we can have some kind of lookup table instead of this
-        if (playerDetails[player1].actualMove == Move.Rock) {
-            if (playerDetails[player2].actualMove == Move.Paper) {
-                winner = player2;
-            } else if (playerDetails[player2].actualMove == Move.Scissor){
-                winner = player1;
-            }
-        } else if (playerDetails[player1].actualMove == Move.Paper) {
-            if (playerDetails[player2].actualMove == Move.Rock) {
-                winner = player1;
-            } else if (playerDetails[player2].actualMove == Move.Scissor) {
-                winner = player2;
-            }
-        } else {
-            if (playerDetails[player2].actualMove == Move.Rock) {
-                winner = player2;
-            } else if (playerDetails[player2].actualMove == Move.Paper) {
-                winner = player1;
-            }
-        }
-        
-        if (winner != 0) {
-            ChangeState();
-            amountWon = playerDetails[player1].moneyWagered + playerDetails[player2].moneyWagered;
-            cleanup();    
-            emit LogWinner(winner, amountWon);
-        } else {
-            NewRound();
-            emit LogNewRoundRequired(player1, player2);
-        }
-    }
-    
-    function enroll() public payable {
-        require(state == GameState.Enrollment);
-        require(!playerDetails[msg.sender].enrolled);
-        require(msg.value >= minWager);
-        
-        playerAddresses[enrolledCount++] = msg.sender;
-        playerDetails[msg.sender] = Player(msg.sender, bytes32(0), Move.None ,msg.value, true, false, false);
-        if (enrolledCount == 2) {
-            ChangeState();
-        }
-        emit LogPlayerEnrolled(msg.sender, msg.value);
-    }
-    
-    function sendSecretMove(bytes32 secretMove) public {
-        require(state == GameState.LockMove);
-        require(playerDetails[msg.sender].enrolled);
-        require(!playerDetails[msg.sender].locked);
+    function issueChallenge(bytes32 secretMove) public payable validAmountOnly returns (uint256 gameID) {
         require(!usedSecrets[secretMove]);
-        
-        playerDetails[msg.sender].hashedMove = secretMove;
-        playerDetails[msg.sender].locked = true;
         usedSecrets[secretMove] = true;
-        lockedCount++;
-        if (lockedCount == 2) {
-            ChangeState();
-        }
-        emit LogSecretMoveLocked(msg.sender, secretMove);
+        
+        gameID = numOfGames++;
+        games[gameID] = Game(GameState.ChallengeIssued, block.number, 0, 0, msg.sender, msg.value, secretMove, 0, 0, Move.None);
+        emit LogChallengeIssued(gameID, msg.sender, msg.value);
     }
     
-    function revealMove(uint256 random, uint8 move) public {
-        require(state == GameState.RevealMove);
-        require(playerDetails[msg.sender].locked);
-        require(!playerDetails[msg.sender].revealed);
-        require(move > 0 && move < 4);
-        require(playerDetails[msg.sender].hashedMove == keccak256(abi.encode(random,move)));
-
+    function acceptChallenge(uint256 gameID, Move move) public payable validAmountOnly {
+        require(games[gameID].state == GameState.ChallengeIssued);
+        require(move > Move.None && move <= Move.Scissors);
+        require(block.number - games[gameID].challengeIssuedTime < timeout);
         
-        playerDetails[msg.sender].actualMove = Move(move);
-        playerDetails[msg.sender].revealed = true;
-        revealedCount++;
-        emit LogMoveRevealed(msg.sender, Move(move));
+        games[gameID].acceptor = msg.sender;
+        games[gameID].acceptorAmount = msg.value;
+        games[gameID].acceptorMove = move;
+        games[gameID].state = GameState.ChallengeAccepted;
+        games[gameID].ChallengeAcceptedTime = block.number;
+        emit LogChallengeAccepted(gameID, msg.sender, msg.value);
+    }
+    
+    function decideWinner(uint256 gameID, Move challengerMove, uint256 random) public returns (bool status){
+        require(games[gameID].state == GameState.ChallengeAccepted);
+        require(msg.sender == games[gameID].challenger);
+        require(games[gameID].challengerSecretMove == keccak256(abi.encode(random, challengerMove)));
         
-        if(revealedCount == 2) {
-            checkWinner();    
+        if (games[gameID].acceptorMove == Move.Rock) {
+            if ( challengerMove == Move.Paper) {
+                games[gameID].winner = games[gameID].challenger;
+            } else if (challengerMove == Move.Scissors) {
+                games[gameID].winner = games[gameID].acceptor;
+            }
+        } else if (games[gameID].acceptorMove == Move.Paper) {
+            if ( challengerMove == Move.Rock) {
+                games[gameID].winner = games[gameID].acceptor;
+            } else if (challengerMove == Move.Scissors) {
+                games[gameID].winner = games[gameID].challenger;
+            }
+        } else if (games[gameID].acceptorMove == Move.Scissors) {
+            if ( challengerMove == Move.Rock) {
+                games[gameID].winner = games[gameID].challenger;
+            } else if (challengerMove == Move.Paper) {
+                games[gameID].winner = games[gameID].acceptor;
+            }
+        }
+        
+        if (games[gameID].winner != 0) {
+            games[gameID].state = GameState.WinnerDecided;
+            status = true;
+        } else {
+            games[gameID].state = GameState.NoWinner;
+            status = false;
         }
     }
     
-    function withdrawIfWinner() public winnerOnly {
-        require(state == GameState.WinnerDecided);
+    function claimWinningAmount(uint256 gameID) public {
+        require(games[gameID].state == GameState.WinnerDecided);
+        require(msg.sender == games[gameID].winner);
         
-        ChangeState();
-        uint256 amount = amountWon;
-        amountWon = 0;
-        winner = 0;
-        emit LogAmountWithdrawn(msg.sender, amount);
-
-        msg.sender.transfer(amount);        
+        uint256 amount = games[gameID].challengerAmount + games[gameID].acceptorAmount;
+        games[gameID].challengerAmount = 0;
+        games[gameID].acceptorAmount = 0;
+        games[gameID].state = GameState.GameOver;
+        emit LogClaimedWinningAmount(gameID, msg.sender, amount);
+        msg.sender.transfer(amount);
     }
-
+    
+    //Challenger should be able to withdraw his wagered amount if no one responded to
+    //his challenge within the timeout period (100 blocks from the creation of the challenge).
+    function withdrawIfChallengeUnanswered(uint256 gameID) public {
+        require(games[gameID].state == GameState.ChallengeIssued);
+        require(msg.sender == games[gameID].challenger);
+        require(block.number - games[gameID].challengeIssuedTime >= timeout);
+        
+        games[gameID].state = GameState.GameOver;
+        uint256 amount = games[gameID].challengerAmount;
+        games[gameID].challengerAmount = 0;
+        emit LogWithdrawnByChallenger(gameID, msg.sender, amount);
+        msg.sender.transfer(amount);
+    }
+    
+    //If challenger does not reveal his move within the timeout period after his challenge
+    //accepted, the acceptor can collect all the money as penalty.
+    function withdrawIfChallengerForfeited(uint256 gameID) public {
+        require(games[gameID].state == GameState.ChallengeAccepted);
+        require(msg.sender == games[gameID].acceptor);
+        require(block.number - games[gameID].ChallengeAcceptedTime >= timeout);
+        
+        uint256 amount = games[gameID].challengerAmount + games[gameID].acceptorAmount;
+        games[gameID].challengerAmount = 0;
+        games[gameID].acceptorAmount = 0;
+        games[gameID].state = GameState.GameOver;
+        emit LogWithdrawnByAcceptor(gameID, msg.sender, amount);
+        msg.sender.transfer(amount);
+    }
+    
+    //if the game ended in a draw
+    function reclaimWageredAmount(uint256 gameID) public {
+        require(games[gameID].state == GameState.NoWinner);
+        require(msg.sender == games[gameID].challenger || msg.sender == games[gameID].acceptor);
+        
+        uint256 amount = 0;
+        if (msg.sender == games[gameID].challenger) {
+            amount = games[gameID].challengerAmount;
+            games[gameID].challengerAmount = 0;
+        } else {
+            amount = games[gameID].acceptorAmount;
+            games[gameID].acceptorAmount = 0;
+        }
+        games[gameID].state = GameState.GameOver;
+        emit LogReclaimedWageredAmount(gameID, msg.sender, amount);
+        msg.sender.transfer(amount);
+    }
 }
